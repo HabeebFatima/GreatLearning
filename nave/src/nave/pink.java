25faeaea669b3720109394f4ff24d4dc47c79a7b
Mohammad Tamazar Ali <Mohammad Tamazar Ali@EpicBoss816M>
2022-05-21 07:35:10 +0530

    pink

---
+++ nave/src/nave/pink.java
@@ -0,0 +1,10 @@
+package nave;
+
+public class pink {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello i'm pink in color guess my name ..?");
+	}
+
+}
